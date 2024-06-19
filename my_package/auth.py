5bd0eade813481a5718301b0a4d0346ed43d9d26
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-19 13:09:09 +0500

    fixed env variables

--- my_package/auth.py
+++ my_package/auth.py
@@ -31,7 +31,7 @@ from .database import SessionLocal
 
 SECRET_KEY = (os.getenv("SECRET_KEY"))
 ALGORITHM = (os.getenv("ALGORITHM"))
-ACCESS_TOKEN_EXPIRE_MINUTES = (os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 # Dependency
 def get_db():
