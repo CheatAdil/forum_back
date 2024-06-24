858a0fc1a5408f2b689e8afdda338e8cbb0b0552
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-24 17:43:33 +0500

    Update schemas.py

--- my_package/entities/schemas.py
+++ my_package/entities/schemas.py
@@ -1,5 +1,22 @@
 from pydantic import BaseModel
 
+class UserBase(BaseModel):
+    user_name: str
+class UserCreate(UserBase):
+    user_password: str
+    user_email: str
+class UserUpdate(UserCreate):
+    user_id: int
+class User(UserBase):
+    user_id: int
+    user_password: str
+    user_email: str
+    #user_registry_date: time.struct_time
+
+    class Config:
+        orm_mode = True
+
+
 class CategoryBase(BaseModel):
     category_name: str
 class CategoryCreate(CategoryBase):
