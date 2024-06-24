f8b4ffbc6258a554da528aded3c5714dcf8ba481
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-24 12:59:49 +0500

    Revert "tf2"
    This reverts commit e21ab1792fe754979e9e73f5baa8cc39680d145b.

--- my_package/password_handler.py
+++
@@ -1,8 +0,0 @@
-from . import hash
-
-def verify_password(plain_password, user_password):
-    return hash.pwd_context.verify(plain_password, user_password)
-
-
-def get_password_hash(password):
-    return hash.get_hash(password)
