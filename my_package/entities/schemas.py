c850a118b0b3d79120bccd88467a885c1858c2e2
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-24 15:57:54 +0500

    cleaned stuff up

--- my_package/entities/schemas.py
+++ my_package/entities/schemas.py
@@ -2,14 +2,11 @@ from pydantic import BaseModel
 
 class UserBase(BaseModel):
     user_name: str
-
 class UserCreate(UserBase):
     user_password: str
     user_email: str
-
 class UserUpdate(UserCreate):
     user_id: int
-
 class User(UserBase):
     user_id: int
     user_password: str
