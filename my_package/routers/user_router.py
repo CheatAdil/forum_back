575ad216d9649c348ffb82e1ae5bfd2bde8cddbc
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 13:52:10 +0500

    Update user_router.py

--- my_package/routers/user_router.py
+++ my_package/routers/user_router.py
@@ -17,7 +17,8 @@ user_router = APIRouter(
 #users
 @user_router.get("/{user_email}", response_model=user_schema.User)
 def check_user(current_user: Annotated[user_schema.User, Depends(get_current_user)]):
-    return current_user.user_id
+    return current_user
+
 @user_router.post("/", response_model=user_schema.User)
 def create_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user: user_schema.UserCreate, db: Session = Depends(get_db) ):
     
@@ -25,6 +26,7 @@ def create_user(current_user: Annotated[user_schema.User, Depends(get_current_us
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
+
 @user_router.get("/", response_model=list[user_schema.User])
 def read_users(current_user: Annotated[user_schema.User, Depends(get_current_user)], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
@@ -34,18 +36,21 @@ def read_users(current_user: Annotated[user_schema.User, Depends(get_current_use
 
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
 @user_router.get("/{user_id}", response_model=user_schema.User)
 def read_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
 @user_router.put("/{user_id}", response_model=user_schema.User)
 def update_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user: user_schema.UserUpdate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, user_email=user.user_email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.update_user(db=db, user=user)
+
 @user_router.delete("/{user_id}", response_model=user_schema.User)
 def delete_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user_id: int, db: Session = Depends(get_db)):
     db_user = crud.delete_user(db, user_id=user_id)
