010e8bd3c1420beec0c50189de00f7c12c40d52a
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 16:23:33 +0500

    category router

--- main.py
+++ main.py
@@ -39,35 +39,6 @@ async def login_for_access_token(
 app.include_router(user_router)
 
 
-#categories
-@app.post("/categories/", response_model=schemas.Category)
-def create_category(current_user: Annotated[schemas.User, Depends(get_current_user)], category: schemas.CategoryCreate, db: Session = Depends(get_db)):
-    db_category = crud.get_category_by_name(db, category_name=category.category_name)
-    if db_category:
-        raise HTTPException(status_code=400, detail="category already registered")
-    return crud.create_category(db=db, category=category)
-@app.get("/categories/", response_model=list[schemas.Category])
-def read_categorys(current_user: Annotated[schemas.User, Depends(get_current_user)], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    categories = crud.get_categorys(db, skip=skip, limit=limit)
-    return categories
-@app.get("/categories/{category_id}", response_model=schemas.Category)
-def read_category(current_user: Annotated[schemas.User, Depends(get_current_user)], category_id: int, db: Session = Depends(get_db)):
-    db_category = crud.get_category(db, category_id=category_id)
-    if db_category is None:
-        raise HTTPException(status_code=404, detail="Category not found")
-    return db_category
-@app.put("/categories/{category_id}", response_model=schemas.Category)
-def update_category(current_user: Annotated[schemas.User, Depends(get_current_user)], category: schemas.CategoryUpdate, db: Session = Depends(get_db)):
-    db_category = crud.get_category_by_name(db, category_name=category.category_name)
-    #if db_category:
-        #raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.update_category(db=db, category=category)
-@app.delete("/categories/{category_id}", response_model=schemas.Category)
-def delete_category(current_user: Annotated[schemas.User, Depends(get_current_user)], category_id: int, db: Session = Depends(get_db)):
-    db_category = crud.delete_category(db, category_id=category_id)
-    #if db_category is None:
-    #    raise HTTPException(status_code=404, detail="Category not found")
-    return db_category
 
 #forums
 @app.post("/forums/", response_model=schemas.Forum)
