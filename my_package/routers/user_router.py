5bdf115626ed3edb6fede64e4338049da732c1e9
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 14:50:44 +0500

    Update user_router.py

--- my_package/routers/user_router.py
+++ my_package/routers/user_router.py
@@ -29,7 +29,7 @@ def read_users(current_user: Annotated[user_schema.User, Depends(get_current_use
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 @user_router.get("/{user_id}", response_model=user_schema.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
