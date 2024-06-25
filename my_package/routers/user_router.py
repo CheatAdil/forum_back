78e8e60084fab2f1bf3d845c62315eda215ad39d
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 14:01:30 +0500

    added prints

--- my_package/routers/user_router.py
+++ my_package/routers/user_router.py
@@ -30,15 +30,16 @@ def create_user(current_user: Annotated[user_schema.User, Depends(get_current_us
 @user_router.get("/", response_model=list[user_schema.User])
 def read_users(current_user: Annotated[user_schema.User, Depends(get_current_user)], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
-    print("##############################")
-    #print("user id = " + current_user.user_id)
-    print("##############################")
-
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 @user_router.get("/{user_id}", response_model=user_schema.User)
 def read_user(current_user: Annotated[user_schema.User, Depends(get_current_user)], user_id: int, db: Session = Depends(get_db)):
+    
+    print("##############################")
+    print("user id = " + user_id)
+    print("##############################")
+    
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
