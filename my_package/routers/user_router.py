266a7c98880c068199d8e26b969e90b783841222
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 15:27:02 +0500

    print in user router

--- my_package/routers/user_router.py
+++ my_package/routers/user_router.py
@@ -4,9 +4,7 @@ from fastapi import Depends, HTTPException, APIRouter
 
 from sqlalchemy.orm import Session
 
-from my_package import crud
 from my_package.cruds import user_crud
-
 from my_package.entities.e_schemas import user_schemas
 
 from my_package.auth import get_current_user
@@ -20,13 +18,14 @@ user_router = APIRouter(
 
 @user_router.post("/", response_model=user_schemas.User)
 def create_user(current_user: Annotated[user_schemas.User, Depends(get_current_user)], user: user_schemas.UserCreate, db: Session = Depends(get_db) ):
-
     db_user = user_crud.get_user_by_email(db, user_email=user.user_email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return user_crud.create_user(db=db, user=user)
 @user_router.get("/", response_model=list[user_schemas.User])
 def read_users(current_user: Annotated[user_schemas.User, Depends(get_current_user)], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+
+    print("type of current user = " + typeof(current_user))
 
     users = user_crud.get_users(db, skip=skip, limit=limit)
     return users
