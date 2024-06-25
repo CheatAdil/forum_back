470b3b4fa4bc00fac80b93a758ef2d4bd5b4ea10
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-25 15:43:57 +0500

    Update user_router.py

--- my_package/routers/user_router.py
+++ my_package/routers/user_router.py
@@ -26,7 +26,7 @@ def create_user(current_user: Annotated[user_schemas.User, Depends(get_current_u
 @user_router.get("/", response_model=list[user_schemas.User])
 def read_users(current_user: Annotated[models.User, Depends(get_current_user)], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
-    print("type of current user = " + type(current_user))
+    print("type of current user = " + (current_user))
 
     users = user_crud.get_users(db, skip=skip, limit=limit)
     return users
