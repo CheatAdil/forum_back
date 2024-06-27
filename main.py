6fd5dfacc853f265ef95e5f7d844b18c4246b74f
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-27 14:52:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from datetime import timedelta
-from typing import Annotated
+from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from sqlalchemy.orm import Session
-
-from my_package.cruds.user_crud import get_user_by_email
-from my_package.entities import models, tokens
-from my_package.auth import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token
-from my_package.database import get_db, engine
+from my_package.entities import models
+from my_package.database import engine
 
 from my_package.routers.user_router import user_router
 from my_package.routers.category_router import category_router
@@ -21,7 +15,6 @@ from my_package.routers.token_router import token_router
 models.Base.metadata.create_all(bind=engine)
 #yes
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.include_router(token_router)
 app.include_router(user_router)
@@ -29,4 +22,3 @@ app.include_router(category_router)
 app.include_router(forum_router)
 app.include_router(forums_and_admins_router)
 app.include_router(forum_posts_router)
-
