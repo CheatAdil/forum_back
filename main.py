f5a84f284e10fcfef74fb0bcd215e0c1dd0467e2
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-06-27 14:56:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-
-
-from my_package.entities import models
-from my_package.database import engine
 
 from my_package.routers.user_router import user_router
 from my_package.routers.category_router import category_router
@@ -12,8 +7,6 @@ from my_package.routers.forums_and_admins_router import forums_and_admins_router
 from my_package.routers.forum_posts_router import forum_posts_router
 from my_package.routers.token_router import token_router
 
-models.Base.metadata.create_all(bind=engine)
-#yes
 app = FastAPI()
 
 app.include_router(token_router)
