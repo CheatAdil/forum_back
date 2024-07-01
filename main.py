f15c5cc9a2f4f59e59ec174b8156a48c3ea78a91
Adil T <43438735+CheatAdil@users.noreply.github.com>
2024-07-01 13:22:05 +0500

    websocket chat from documentation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from routers.forum_router import forum_router
 from routers.forums_and_admins_router import forums_and_admins_router
 from routers.forum_posts_router import forum_posts_router
 from routers.token_router import token_router
+from websocket.websocket import websocket_chat_router
 
 app = FastAPI()
 
@@ -15,3 +16,4 @@ app.include_router(category_router)
 app.include_router(forum_router)
 app.include_router(forums_and_admins_router)
 app.include_router(forum_posts_router)
+app.include_router(websocket_chat_router)
